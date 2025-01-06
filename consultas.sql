eea2abaddaec989c96aa4214516f351f302f1396
Guilherme Vianna de Moura <88854722+GuilhermeViannaMoura@users.noreply.github.com>
2025-01-06 11:42:42 -0300

    att consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,37 +1,37 @@
 -- 1. Selecionar todos os usuários com seus endereços, incluindo usuários sem endereço
 SELECT l.*, a.*
-FROM `Login` l
-LEFT JOIN `Address` a ON l.id = a.id;
+FROM "Login" l
+LEFT JOIN "Address" a ON l.id = a.id;
 
 -- 2. Contar o número de redefinições de senha por usuário e mostrar apenas usuários com mais de uma redefinição
-SELECT l.email, COUNT(r.id) as reset_count
-FROM `Login` l
-JOIN `ResetPassword` r ON l.id = r.login_id
+SELECT l.email, COUNT(r.id) reset_count
+FROM "Login" l
+JOIN "ResetPassword" r ON l.id = r.login_id
 GROUP BY l.email
-HAVING reset_count > 1;
+HAVING COUNT(r.id) > 1;
 
 -- 3. Selecionar todos os usuários que têm um endereço em São Paulo e que não estão desativados
 SELECT l.*, a.*
-FROM `Login` l
-JOIN `Address` a ON l.id = a.id
+FROM "Login" l
+JOIN "Address" a ON l.id = a.id
 WHERE a.city = 'São Paulo' AND l.deactivated = false;
 
 -- 4. Contar o número de usuários por cidade
 SELECT a.city, COUNT(l.id) as user_count
-FROM `Login` l
-JOIN `Address` a ON l.id = a.id
+FROM "Login" l
+JOIN "Address" a ON l.id = a.id
 GROUP BY a.city;
 
 -- 5. Selecionar todos os endereços que foram atualizados nos últimos 7 dias e os usuários correspondentes
 SELECT a.*, l.*
-FROM `Address` a
-JOIN `Login` l ON a.id = l.id
-WHERE a.updatedAt >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY);
+FROM "Address" a
+JOIN "Login" l ON a.id = l.id
+WHERE a."updatedAt" >= current_date - INTERVAL '7 days';
 
 -- 6. Selecionar todos os endereços dos estacionamentos
 SELECT a.*
-FROM `Address` a
-JOIN `Login` l ON a.id = l.id
+FROM "Address" a
+JOIN "Login" l ON a.id = l.id
 WHERE l.role = 'PARKING_LOT';
 
 -- 7. Obter todos os horários de um estacionamento
@@ -42,18 +42,18 @@ SELECT
     tobj.user_text AS user_text,
     tobj.manager_text AS manager_text
 FROM 
-    `ParkingLot` pl
+    "ParkingLot" pl
 JOIN 
-    `ReservationTimelineObjects` rto ON pl.id = rto.reservation_id
+    "ReservationTimelineObjects" rto ON pl.id = rto.reservation_id
 JOIN 
-    `TimelineObject` tobj ON rto.status_code = tobj.id
+    "TimelineObject" tobj ON rto.status_code = tobj.id
 WHERE 
     pl.id = 'id_do_estacionamento';
 
 -- 8. Obter todas as reservas de um estacionamento que estão aguardando serem confirmadas
 SELECT *
 FROM 
-  `Reservation` r
+  "Reservation" r
 WHERE 
     r.status = 'WAITING' AND r.parking_lot_id = 'id_do_estacionamento';
 
@@ -61,12 +61,12 @@ WHERE
 SELECT 
     c.id AS convenience_id,
     c.name AS convenience_name,
-    cpl.createdAt AS added_at,
-    cpl.updatedAt AS updated_at
+    cpl."createdAt" AS added_at,
+    cpl."updatedAt" AS updated_at
 FROM 
-    `Conveniences` c
+    "Conveniences" c
 JOIN 
-    `ConveniencesOnParkingLots` cpl ON c.id = cpl.conveniences_id
+    "ConveniencesOnParkingLots" cpl ON c.id = cpl.conveniences_id
 WHERE 
     cpl.parking_lot_id = 'id_do_estacionamento';
 
@@ -76,7 +76,7 @@ SELECT
     r.conductor_phone,
     r.conductor_name,
     r.vehicle_id,
-    v.license_plate,
+    v.plate,
     v.model,
     v.color,
     u.id AS user_id,
@@ -90,15 +90,15 @@ SELECT
     r.checkin_code,
     r.checkout_code,
     r.internal_reference,
-    r.createdAt,
-    r.updatedAt,
+    r."createdAt",
+    r."updatedAt",
     r.parking_spot_id,
     r.parking_spot_used
 FROM 
-    `Reservation` r
+    "Reservation" r
 JOIN 
-    `Vehicle` v ON r.vehicle_id = v.id
+    "Vehicle" v ON r.vehicle_id = v.id
 JOIN 
-    `User` u ON v.user_id = u.id
+    "User" u ON v.user_id = u.id
 WHERE 
     u.id = 'user_id';
